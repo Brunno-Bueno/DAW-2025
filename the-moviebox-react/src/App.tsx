e678405043efce1ce99f9a72e2ea3eb7cba7009b
Bruno <crimatou364@gmail.com>
2025-10-23 21:56:31 -0300

    Commit Aula 2

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,4 +1,5 @@
-import { Header } from "./components/layout/Header"
+import { Footer } from "./components/layout/footer.tsx"
+import { Header } from "./components/layout/header.tsx"
 
 function App() {
 
@@ -6,6 +7,8 @@ function App() {
   return (
     <>
       <Header/>
+      <main></main>
+      <Footer/>
     </>
   )
 }
